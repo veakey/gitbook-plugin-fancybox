6a0f09e190df4b78ed99ef41ea1991eeceb3ad32
veakey <kim.dara@gmail.com>
2018-04-06 17:44:46 +0200

    test on detecting change flag

--- assets/plugin.js
+++ assets/plugin.js
@@ -2,11 +2,45 @@ require([
     'gitbook'
 ], function(gitbook) {
     gitbook.events.on('start', function(e, config) {
-        $('.fancybox').fancybox($.extend({
+        /*$('.fancybox-md').fancybox({
+
+        }, config.fancybox);*/
+        //$('.fancybox-md').unbind('click');
+        $('.fancybox-md').on('click', function(e){
+            e.preventDefault();
+            //alert("asdfasdf");
+            const $me = $(this);
+            $(this).fancybox($.extend({
+                'loop': false,
+                'afterLoad' : function() {
+                    console.log(this);
+                    //this.type = 'iframe';
+
+                    const me = this;
+
+                    $.get($me.attr('href'), function(content){
+                        /*me.renderBlock('markdown', content)
+                        .then(function (data) {
+                            //return format(data, title)
+                            me.content = data;
+                        });*/
+                        me.content = content;
+                    })
+
+
+
+                    //this.content = 'asdfasdf';
+
+                }
+            },config.fancybox));
+            //$('#myModal').modal('show').find('.modal-body').load($(this).attr('href'));
+        });
+        /*$('.fancybox-md').fancybox($.extend({
                 'loop': false,
             	'afterLoad' : function() {
-					this.title = (this.index + 1) + '/' + this.group.length + (this.title ? ' - ' + this.title : '');
+                    this.content="asdfasdf";
+					//this.title = (this.index + 1) + '/' + this.group.length + (this.title ? ' - ' + this.title : '');
 				}
-        },config.fancybox));
+        },config.fancybox));*/
     });
 });
